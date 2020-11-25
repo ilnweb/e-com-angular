b5aa3111410be10ae63023b35da9c443ddb655d2
iliyan <iliyan.tsachev@ilnweb.com>
2020-11-25 12:15:07 +0100

    generating new components and pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,7 +9,13 @@ import { NavComponent } from './components/nav/nav.component';
 import { CardComponent } from './components/card/card.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon'
+import {MatIconModule} from '@angular/material/icon';
+import { UserProfileComponent } from './pages/user-profile/user-profile.component';
+import { CreateProductComponent } from './pages/create-product/create-product.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component'
 
 @NgModule({
   declarations: [
@@ -17,6 +23,12 @@ import {MatIconModule} from '@angular/material/icon'
     HomeComponent,
     NavComponent,
     CardComponent,
+    UserProfileComponent,
+    CreateProductComponent,
+    LoginComponent,
+    RegisterComponent,
+    HeaderComponent,
+    FooterComponent,
    
   ],
   imports: [
