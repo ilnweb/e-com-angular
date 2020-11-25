8bd67404bbd7a3780a6d322fefe46cf0167b6e5f
iliyan <iliyan.tsachev@ilnweb.com>
2020-11-25 11:43:36 +0100

    implemeting first routs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
+import { HomeComponent } from './pages/home/home.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavComponent } from './nav/nav.component';
-import { CardComponent } from './card/card.component';
+import { NavComponent } from './components/nav/nav.component';
+import { CardComponent } from './components/card/card.component';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatButtonModule } from '@angular/material/button';
 import {MatIconModule} from '@angular/material/icon'
